85b998405e81ab9e5867161f846a8623b8bea406
Sucipto-Abdullah <abdulcipto1@gmail.com>
2026-02-17 13:13:55 +0700

    awd

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 async function loadComponent(element, file) {
     const response = await fetch(file);
     const html = await response.text();
-    document.getElementById(element).html = html;
+    $('body').html(html);
 }
 
 loadComponent('nav', './view/component/navbar.html');
